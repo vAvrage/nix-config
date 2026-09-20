d718af205c09121416ce42429394885fe0a83a75
vAvrage <vavrage@proton.me>
2026-09-20 15:39:46 +0930

    feat: latest ver. commit

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -4,10 +4,16 @@
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     extest.enable = true;
+    gamescopeSession.enable = false;
 
     extraCompatPackages = [
       pkgs.proton-ge-bin
     ];
+  };
+
+  programs.gamescope = {
+    enable = true;
+    capSysNice = false;
   };
 
   programs.gamemode.enable = true;
