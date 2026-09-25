a02bd7bbb1a15a85b518346a12541c7bcd5d4728
vAvrage <vavrage@proton.me>
2026-09-25 11:17:16 +0930

    feat: better cursor and border

--- modules/home/cursor.nix
+++ modules/home/cursor.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }: {
   home.pointerCursor = {
     enable = true;
-    package = pkgs.vanilla-dmz;
-    name = "Vanilla-DMZ";
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Ice";
     size = 24;
 
     gtk.enable = true;
@@ -10,7 +10,7 @@
   };
 
   home.sessionVariables = {
-    XCURSOR_THEME = "Vanilla-DMZ";
+    XCURSOR_THEME = "Bibata-Modern-Ice";
     XCURSOR_SIZE = "24";
   };
 }
